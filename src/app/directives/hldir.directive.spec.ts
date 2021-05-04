9f1d73140e3551293cb31d6aae3562eaa5e5d307
Vilensciaga <73659500+Vilensciaga@users.noreply.github.com>
2021-05-04 12:43:20 -0400

    Delete src directory

--- src/app/directives/hldir.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HldirDirective } from './hldir.directive';
-
-describe('HldirDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HldirDirective();
-    expect(directive).toBeTruthy();
-  });
-});
