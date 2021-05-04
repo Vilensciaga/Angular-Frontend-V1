014b2aba73da1f76dffbd3797e906dc31d4ff3a3
Vilensciaga <73659500+Vilensciaga@users.noreply.github.com>
2021-05-04 12:42:30 -0400

    Add files via upload

---
+++ src/app/directives/hldir.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HldirDirective } from './hldir.directive';
+
+describe('HldirDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HldirDirective();
+    expect(directive).toBeTruthy();
+  });
+});
